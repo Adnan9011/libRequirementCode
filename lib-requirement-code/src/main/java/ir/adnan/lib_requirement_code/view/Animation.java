f32ee7caae7558f9ed3e5ef12214c72514f27ca8
Adnan abdollah zaki <adnan9011@gmail.com>
2017-09-04 15:17:47 +0430

    2Sh 96/06/13 Version 0.0.41 + Add repeative animation

--- lib-requirement-code/src/main/java/ir/adnan/lib_requirement_code/view/Animation.java
+++ lib-requirement-code/src/main/java/ir/adnan/lib_requirement_code/view/Animation.java
@@ -24,6 +24,13 @@ public class Animation {
                 .duration(durationSecound * 1000)
                 .playOn(view);
     }
+    public static void animation(Techniques techniques, int durationSecound, View view , int repeat , int repeatMode) {
+        YoYo.with(techniques)
+                .duration(durationSecound * 1000)
+                .repeat(repeat)
+                .repeatMode(repeatMode)
+                .playOn(view);
+    }
 
     //
     public static void fadeInfadeOutInfinite(View view ) {
